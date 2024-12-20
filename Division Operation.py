1bdc15d86d289aba67522e8640e7c28e47358e1d
octane312 <abidchettuthara@gmail.com>
2024-12-20 12:35:56 +0530

    Update Division Operation.py

--- Division Operation.py
+++ Division Operation.py
@@ -1,4 +1,4 @@
-a = int(input())
-b = int(input())
-print(a//b)
-print(a/b)
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+print("The integer part of result is",a//b)
+print("The result is",a/b)
